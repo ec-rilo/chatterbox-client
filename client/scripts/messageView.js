3de9d1d6107d492032ca469767568e1b3dd71b51
Edgar Carrillo <ecarrillo046@gmail.com>
2022-03-05 10:01:31 -0800

    create html for chat

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -6,12 +6,19 @@ var MessageView = {
   // here: https://underscorejs.org/#template.
   // TODO: Update this template accordingly.
   render: _.template(`
-      <!--
       <div class="chat">
-        <div class="username"></div>
-        <div></div>
+        <div class="profile-pic">
+          <%= username.charAt(0).toUpperCase() %>
+        </div>
+        <div class="information-container">
+          <div class="user-info-container">
+            <p><span class="room"><%= roomname %> <span class="username">@<%= username %></span></span></p>
+          </div>
+          <div class="text-container">
+            <p><%= text %></p>
+          </div>
+        </div>
       </div>
-      -->
     `)
 
 };
